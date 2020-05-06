bb818e24abd65ea4077199c138ae38f40720970a
Asher Azeem <asherazeem25@gmail.com>
2020-05-06 11:51:28 +0500

    Add functions View Appear or DisAppear

--- Udar/ViewController.swift
+++ Udar/ViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         print(#function)
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
 }
 
